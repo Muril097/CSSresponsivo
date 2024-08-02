b42903ba8f7a743b2f99005f349fa9553aacfaf0
Murilo Braga <murilobraga3@gmail.com>
2024-08-02 18:10:36 -0300

    comit do exercicio do css responsivo

---
+++ backup/card.js
@@ -0,0 +1,15 @@
+let pizzaJson = [
+    {id:1, name:'Mozzarella', img:'image/muzarela.jpg', price:25.00, sizes:['4 fatias', '6 fatias', '8 fatias'], description:'Molho de tomate, camada dupla de mussarela e orégano'},
+    {id:2, name:'Calabresa', img:'image/calabrese.jpg', price:26.00, sizes:['4 fatias', '6 fatias', '8 fatias'], description:'Molho de tomate, mussarela, calabresa fatiada, cebola e orégano'},
+    {id:3, name:'Quatro Queijos', img:'image/4queijos.jpg', price:28.00, sizes:['4 fatias', '6 fatias', '8 fatias'], description:'Molho de tomate, camadas de mussarela, provolone, parmessão, gorgonzola e orégano'},
+    {id:4, name:'Verona', img:'image/verona.jpg', price:30.00, sizes:['4 fatias', '6 fatias', '8 fatias'], description:'Creme de brócolis, muçarela, ricota de cabra, brócolis e pecorino'},
+    {id:5, name:'Portuguesa', img:'image/portuguesa.jpg', price:29.00, sizes:['4 fatias', '6 fatias', '8 fatias'], description:'Molho de tomate, mussarela, presunto, ovos, cebolas, pimentão, azeitona  e orégano'},
+    {id:6, name:'Napolitana', img:'image/napolitana.jpg', price:35.00, sizes:['4 fatias', '6 fatias', '8 fatias'], description:'Molho de tomate, mussarela, parmesão, mangericão, azeitona e orégano'},
+];
+
+let refriJson = [
+    {id:7, name:'Coca-cola', img:'image/coca.png', price:3.00, sizes:['350ml', '600ml', '2l'], description:'Refrigerante sabor cola gelado'},
+    {id:8, name:'Fanta uva', img:'image/fanta uva.png', price:3.00, sizes:['350ml', '600ml', '2l'], description:'Refrigerante sabor uva gelado'},
+    {id:9, name:'Guaraná', img:'image/guaraná.png', price:3.00, sizes:['350ml', '600ml', '2l'], description:'Refrigerante sabor guaraná gelado'},
+    {id:10, name:'Sprite', img:'image/sprite.png', price:3.00, sizes:['350ml', '600ml', '2l'], description:'Refrigerante sabor limão gelado'},
+];
